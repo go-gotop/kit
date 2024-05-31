ee1bb4b87a218a5e32859b3cc57fdb8cad9d7fa1
hom <hcclinux@gmail.com>
2024-05-31 14:57:02 +0800

    chore: remove Position StrategyID

--- exchange/event.go
+++ exchange/event.go
@@ -5,12 +5,11 @@ import (
 )
 
 const (
-	OrderServiceType = "OderService"
+	OrderServiceType   = "OderService"
 	CapitalServiceType = "CapitalService"
 )
 
 type DeadLetterEvent struct {
-
 }
 
 type NotifyEvent struct {
@@ -21,7 +20,8 @@ type NotifyEvent struct {
 }
 
 type CreateOrderEvent struct {
-	ID             string
+	// ID 交易ID
+	TransactionID  string
 	Timestamp      int64
 	ClientOrderID  string
 	Symbol         string
@@ -34,96 +34,97 @@ type CreateOrderEvent struct {
 }
 
 type OrderResultEvent struct {
-	// ID 事件ID
-	ID                string
+	// ID 交易ID
+	TransactionID string
 	// Exchange 交易所
-	Exchange          string
+	Exchange string
 	// ClientOrderID 自定义客户端订单号
-	ClientOrderID     string
+	ClientOrderID string
 	// Symbol 交易对
-	Symbol            string
+	Symbol string
 	// OrderID 交易所订单号
-	OrderID           string
+	OrderID string
 	// FeeAsset 手续费资产
-	FeeAsset          string
+	FeeAsset string
 	// TransactionTime 交易时间
-	TransactionTime   int64
+	TransactionTime int64
 	// IsTaker 是否是挂单方
-	IsMaker           bool
+	IsMaker bool
 	// Instrument 种类
-	Instrument        InstrumentType
+	Instrument InstrumentType
 	// ExecutionType 本次订单执行类型:NEW，FILLED，CANCELED，REJECTED，EXPIRED
-	ExecutionType	  OrderState
+	ExecutionType OrderState
 	// State 当前订单执行类型:NEW，FILLED，CANCELED，REJECTED，EXPIRED
-	State             OrderState
+	State OrderState
 	// PositionStatus 仓位状态: OpenPosition，HoldingPosition，ClosingPosition，PositionClosed
-	Status            PositionStatus
+	Status PositionStatus
 	// PositionSide LONG，SHORT
-	PositionSide      PositionSide
+	PositionSide PositionSide
 	// SideType BUY，SELL
-	Side              SideType
+	Side SideType
 	// OrderType LIMIT，MARKET
-	Type              OrderType
+	Type OrderType
 	// Volume 原交易数量
-	Volume            decimal.Decimal
+	Volume decimal.Decimal
 	// Price 交易价格
-	Price             decimal.Decimal
+	Price decimal.Decimal
 	// LatestVolume 最新交易数量
-	LatestVolume      decimal.Decimal
+	LatestVolume decimal.Decimal
 	// FilledVolume 已成交数量
-	FilledVolume      decimal.Decimal
+	FilledVolume decimal.Decimal
 	// LatestPrice 最新交易价格
-	LatestPrice       decimal.Decimal
+	LatestPrice decimal.Decimal
 	// FeeCost 手续费
-	FeeCost           decimal.Decimal
+	FeeCost decimal.Decimal
 	// FilledQuoteVolume 已成交金额
 	FilledQuoteVolume decimal.Decimal
 	// LatestQuoteVolume 最新成交金额
 	LatestQuoteVolume decimal.Decimal
 	// QuoteVolume 交易金额
-	QuoteVolume       decimal.Decimal
+	QuoteVolume decimal.Decimal
 	// AvgPrice 平均成交价格
-	AvgPrice          decimal.Decimal
+	AvgPrice decimal.Decimal
 }
 
 type StrategySignalEvent struct {
-	// ID 策略ID
-	ID            string
+	// ID 交易ID
+	TransactionID string
 	// AccountID 账户ID
-	AccountID     string
+	AccountID string
 	// Timestamp 当前时间戳
-	Timestamp     int64
+	Timestamp int64
 	// ClientOrderID 自定义客户端订单号
 	ClientOrderID string
 	// TimeInForce GTC，IOC，FOK，GTX，GTD
-	TimeInForce   TimeInForce
+	TimeInForce TimeInForce
 	// SideType BUY，SELL
-	Side          SideType
+	Side SideType
 	// OrderType LIMIT，MARKET
-	OrderType     OrderType
+	OrderType OrderType
 	// PositionSide LONG，SHORT
-	PositionSide  PositionSide
+	PositionSide PositionSide
 	// Symbol 交易对
-	Symbol        Symbol
+	Symbol Symbol
 	// Size 头寸数量
-	Size          decimal.Decimal
+	Size decimal.Decimal
 	// Price 交易价格
-	Price         decimal.Decimal
+	Price decimal.Decimal
 }
 
 type StrategyStatusEvent struct {
-	ID     string
-	Symbol Symbol
-	Status StrategyStatus
+	// ID 交易ID
+	TransactionID string
+	Symbol        Symbol
+	Status        StrategyStatus
 }
 
 type TradeEvent struct {
-	TradedAt int64
-	TradeID  string
-	Symbol   string
-	Exchange string
-	Size     decimal.Decimal
-	Price    decimal.Decimal
-	Side     SideType
+	TradedAt   int64
+	TradeID    string
+	Symbol     string
+	Exchange   string
+	Size       decimal.Decimal
+	Price      decimal.Decimal
+	Side       SideType
 	Instrument InstrumentType
 }
