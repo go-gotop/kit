5c8957e1a397ca45e9aed646a5d2ab3c5b9e96ec
lichao <1962242900@qq.com>
2024-06-20 12:10:18 +0800

    chore: update log

--- cust/log/helep.go
+++ cust/log/helep.go
@@ -18,34 +18,34 @@ type LogEntry struct {
 }
 
 // customLogger 是一个自定义的日志记录器，用于格式化日志时间戳。
-type customLogger struct {
-	logger log.Logger
-}
-
-func (c *customLogger) Log(level log.Level, keyvals ...interface{}) error {
-	var timestamp string
-	for i := 0; i < len(keyvals); i += 2 {
-		if keyvals[i] == "ts" {
-			if ts, ok := keyvals[i+1].(time.Time); ok {
-				// 转换时间为上海时间
-				loc, _ := time.LoadLocation("Asia/Shanghai")
-				timestamp = ts.In(loc).Format("2006-01-02T15:04:05Z07:00")
-				keyvals[i+1] = timestamp
-			}
-		}
-	}
-	err := c.logger.Log(level, keyvals...)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func newCustomLogger() log.Logger {
-	return &customLogger{
-		logger: log.NewStdLogger(os.Stdout),
-	}
-}
+//type customLogger struct {
+//	logger log.Logger
+//}
+//
+//func (c *customLogger) Log(level log.Level, keyvals ...interface{}) error {
+//	var timestamp string
+//	for i := 0; i < len(keyvals); i += 2 {
+//		if keyvals[i] == "ts" {
+//			if ts, ok := keyvals[i+1].(time.Time); ok {
+//				// 转换时间为上海时间
+//				loc, _ := time.LoadLocation("Asia/Shanghai")
+//				timestamp = ts.In(loc).Format("2006-01-02T15:04:05Z07:00")
+//				keyvals[i+1] = timestamp
+//			}
+//		}
+//	}
+//	err := c.logger.Log(level, keyvals...)
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
+//
+//func newCustomLogger() log.Logger {
+//	return &customLogger{
+//		logger: log.NewStdLogger(os.Stdout),
+//	}
+//}
 
 // RedisHandler 是一个log.Logger，将日志存储到Redis。
 type RedisHandler struct {
@@ -92,7 +92,7 @@ func (h *RedisHandler) Log(level log.Level, keyvals ...interface{}) error {
 		}
 	}
 	nano := time.Now().UnixNano()
-	key := fmt.Sprintf("log:%d", nano)
+	key := fmt.Sprintf("{log}:%d", nano)
 	entry := &LogEntry{
 		Service:   h.serviceName,
 		Level:     levelField,
@@ -137,12 +137,12 @@ func newRedisClient(addr, passwd string, db int32) *redis.Client {
 func NewLogger(env, svcName, addr, passwd string, db int32) *MultiLogger {
 	var multi *MultiLogger
 	if env == "PRD" {
-		stdout := newCustomLogger()
+		stdout := log.NewStdLogger(os.Stdout)
 		handler := newRedisHandler(newRedisClient(addr, passwd, db), svcName)
 		multi = newMultiLogger(stdout, handler)
 	} else {
-		stdout := newCustomLogger()
-		multi = newMultiLogger(stdout)
+		//stdout := newCustomLogger()
+		multi = newMultiLogger(log.NewStdLogger(os.Stdout))
 	}
 	return multi
 }
